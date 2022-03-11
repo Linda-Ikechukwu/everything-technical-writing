a758be6cae77ef57ec581002f3061f01c5e41960
Linda Ikechukwu <32891838+Linda-Ikechukwu@users.noreply.github.com>
2022-03-11 12:48:44 +0100

    Update subscriber count

--- src/components/common/SubscriptionForm.js
+++ src/components/common/SubscriptionForm.js
@@ -8,7 +8,7 @@ const SubscriptionForm = () => {
             </div>
             <div className="sub-form-bottom">
                 <p>
-                    Join over <strong>350+ subscribers just like you</strong>.
+                    Join over <strong>400+ subscribers just like you</strong>.
                     Every two weeks, I'll send you <strong>new articles and expert interviews</strong> published on the blog,
                     so you'd never miss out. I'll also send you a curated list of <strong>other valuable resources on
                     technical writing </strong> across the internet, as well as  <strong>fully-remote technical writing gigs/jobs </strong>
