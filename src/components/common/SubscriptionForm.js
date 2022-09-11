f46a0377a9ca02e351080d6988b4cd2ce0efa742
Linda Ikechukwu <32891838+Linda-Ikechukwu@users.noreply.github.com>
2022-09-12 00:08:47 +0100

    Update SubscriptionForm.js

--- src/components/common/SubscriptionForm.js
+++ src/components/common/SubscriptionForm.js
@@ -8,7 +8,7 @@ const SubscriptionForm = () => {
             </div>
             <div className="sub-form-bottom">
                 <p>
-                    Join over <strong>600+ subscribers just like you</strong>.
+                    Join over <strong>850+ subscribers just like you</strong>.
                     Every two weeks, I'll send you <strong>new articles and expert interviews</strong> published on the blog,
                     so you'd never miss out. I'll also send you a curated list of <strong>other valuable resources on
                     technical writing </strong> across the internet, as well as  <strong>fully-remote technical writing gigs/jobs </strong>
