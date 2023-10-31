dcb9221e4e04c919ca0375891fed64cf5bfabe2c
Linda Ikechukwu <32891838+Linda-Ikechukwu@users.noreply.github.com>
2023-10-31 12:18:32 +0100

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteUrl: `https://www.everythingtechnicalwriting.com`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 21, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `Everything Technical Writing`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Your One Stop Blog For All Things Technical Writing in the Software Industry`,
